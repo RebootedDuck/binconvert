7436ae59c6370a338acaa6f6f8d7049de0f7ebc4
RebooteDuck <rebooteduck@gmail.com>
2024-09-22 14:18:30 +0100

    Improve path detection for tests so they actually work

--- tests/_test.py
+++ tests/_test.py
@@ -1,8 +1,15 @@
+import os
+import sys
+import inspect
 import unittest
-from binConvertClass import binaryOperations
-testsBits = [[0],[0,0,0],[1,0,1],[1,1,1],[1,0,0,1],[0,1,0,1,0,1,1]]
-testBits = [[[0],0],[[0,0,0],0],[[1,0,1],5],[[1,1,1],7],[[1,0,0,1],9],[[0,1,0,1,0,1,1],43]]
 
+currentDir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentDir = os.path.dirname(currentDir)
+sys.path.insert(0, parentDir) 
+
+from binConvertClass import binaryOperations  # noqa: E402
+
+testBits = [[[0],0],[[0,0,0],0],[[1,0,1],5],[[1,1,1],7],[[1,0,0,1],9],[[0,1,0,1,0,1,1],43]]
 
 class testBinaryOperations(unittest.TestCase):
     def testConvertBitsToInt(self):
@@ -14,14 +21,9 @@ class testBinaryOperations(unittest.TestCase):
             else:
                 trueness= False
         self.assertTrue(trueness)
-        #print(int(binaryOperations.convertBinaryToInt([testBits[index][0]])))
-        #print(int(testBits[index][1]))
-        #print(str(testBits[index][0])+" - "+str(testBits[index][1])+" - "+str(valueMatch))
-        print(trueness)
         
     def testConvertIntToBits(self):
         trueness = True
-        
         for i in range(0,1000):
             bits = binaryOperations.convertIntToBits(i)
             convertedInt = binaryOperations.convertBinaryToInt(bits)
@@ -30,10 +32,7 @@ class testBinaryOperations(unittest.TestCase):
                 pass
             else:
                 trueness = False
-        
         self.assertTrue(trueness)
-        print(trueness)
     
-
 if __name__ == '__main__':
     unittest.main()
